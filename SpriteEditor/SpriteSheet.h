3beee4dc3eb7d64bf43a2e2ecd5546b325a20428
William Terry <Tezza48is@aol.com>
2019-01-04 02:39:05 +0000

    Can edit multiple Sprites

--- SpriteEditor/SpriteSheet.h
+++ SpriteEditor/SpriteSheet.h
@@ -1,45 +1,39 @@
 #pragma once
 #include <string>
-#include <vector>
+#include <array>
 
 struct Sprite
 {
-	union
-	{
-		float *_val;
-		struct
-		{
-			float rect[4];
-			float color[4];
-		};
-	};
+	std::array<float, 4> rect;
+	std::array<float, 4> color;
+
 	Sprite()
 	{
-		float def[8] = { 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f };
-		_val = def;
+		rect = std::array<float, 4>({ 0.25, 0.25, 0.5, 0.5 });
+		color = std::array<float, 4>({ 1, 1, 1, 1 });
 	}
 };
 
-struct SpriteSheet
-{
-	std::string texName;
-	std::vector<Sprite> sprites;
-
-	SpriteSheet()
-	{
-		sprites.emplace_back();
-	}
-
-	inline std::vector<const char *> GetListboxItems()
-	{
-		std::vector<const char *> result;
-		for (size_t i = 0; i < sprites.size(); i++)
-		{
-			std::string name = "Sprite " + std::to_string(i+1);
-			auto cs = name.c_str();
-			result.push_back(name.c_str());
-		}
-		return result;
-	}
-};
-
+//struct SpriteSheet
+//{
+//	std::string texName;
+//	std::vector<Sprite> sprites;
+//
+//	SpriteSheet()
+//	{
+//		sprites.emplace_back();
+//	}
+//
+//	inline std::vector<const char *> GetListboxItems()
+//	{
+//		std::vector<const char *> result;
+//		for (size_t i = 0; i < sprites.size(); i++)
+//		{
+//			std::string name = "Sprite " + std::to_string(i+1);
+//			auto cs = name.c_str();
+//			result.push_back(name.c_str());
+//		}
+//		return result;
+//	}
+//};
+//
--- SpriteEditor/main.cpp
+++ SpriteEditor/main.cpp
@@ -3,21 +3,26 @@
 #include "vendor/imgui/imgui_impl_opengl3.h"
 #include <stb/stb_image.h>
 #include <cstdio>
-
+#include <vector>
 #include <GL/glew.h>    // Initialize with glewInit()
 #include <GLFW/glfw3.h>
 
 #include "SpriteSheet.h"
+#include <fstream>
 
 void OnGui();
 
 bool OpenTexture(const char * filename);
+void OpenSprites(const char * filename);
+void SaveSprites(const char * filename);
+bool SerializeSprites(void * data, int idx, const char ** outData);
 
-static GLFWwindow * window;
-static int windowWidth = 1600, windowHeight = 900;
+GLFWwindow * window;
+int windowWidth = 1280, windowHeight = 720;
 GLuint tex;
 
-SpriteSheet * sheet;
+std::vector<Sprite> sprites(1);
+int currentSpriteIndex;
 
 int main(int argc, char ** argv)
 {
@@ -48,10 +53,11 @@ int main(int argc, char ** argv)
 	glewInit();
 
 	ImGui::CreateContext();
-	ImGuiIO & io = ImGui::GetIO();
 	
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init("#version 130");
+
+	ImGui::StyleColorsLight();
 
 	stbi_set_flip_vertically_on_load(false);
 
@@ -61,13 +67,13 @@ int main(int argc, char ** argv)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-	sheet = new SpriteSheet();
-	sheet->texName = "environment-desert.png";
 	OpenTexture("environment-desert.png");
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
+
+		glfwGetWindowSize(window, &windowWidth, &windowHeight);
 
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
@@ -76,28 +82,28 @@ int main(int argc, char ** argv)
 		ImGui::Render();
 
 		glClear(GL_COLOR_BUFFER_BIT);
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClearColor(3.0f, 3.0f, 3.0f, 1.0f);
 
 		glMatrixMode(GL_PROJECTION_MATRIX);
 
 		float aspect = static_cast<float>(windowWidth) / windowHeight;
 
-		glBegin(GL_QUADS);
-		glColor3f(1, 1, 1);
-		glVertex2f(-aspect, -1);
-		glVertex2f(aspect, -1);
-		glVertex2f(aspect, 1);
-		glVertex2f(-aspect, 1);
-		glEnd();
+		//glBegin(GL_QUADS);
+		//glColor3f(1, 1, 1);
+		//glVertex2f(-aspect, -1);
+		//glVertex2f(aspect, -1);
+		//glVertex2f(aspect, 1);
+		//glVertex2f(-aspect, 1);
+		//glEnd();
 
-		glBegin(GL_TRIANGLES);
-		glColor3f(1, 0, 0);
-		glVertex2f(-0.5f, -0.5f);
-		glColor3f(0, 1, 0);
-		glVertex2f(0.0f, 0.5f);
-		glColor3f(0, 0, 1);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		//glBegin(GL_TRIANGLES);
+		//glColor3f(1, 0, 0);
+		//glVertex2f(-0.5f, -0.5f);
+		//glColor3f(0, 1, 0);
+		//glVertex2f(0.0f, 0.5f);
+		//glColor3f(0, 0, 1);
+		//glVertex2f(0.5f, -0.5f);
+		//glEnd();
 
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		glfwSwapBuffers(window);
@@ -111,21 +117,29 @@ int main(int argc, char ** argv)
 
 void OnGui()
 {
-	static bool showOpenDialog = false;
-	{
+	static bool showDemoWindow = false;
+
+	float mainMenubarHeight;
+	{// Main Menubar
 		ImGui::BeginMainMenuBar();
 		if (ImGui::BeginMenu("File"))
 		{
-			if (ImGui::MenuItem("New"))
-			{
-				if(sheet) delete sheet;
-				sheet = new SpriteSheet();
-			}
-			//showOpenDialog = ImGui::MenuItem("Open");
-
-			//showOpenDialog = ImGui::MenuItem("Open");
+			if (ImGui::MenuItem("Quit")) glfwSetWindowShouldClose(window, true);
 			ImGui::EndMenu();
 		}
+
+		if (ImGui::BeginMenu("Edit"))
+		{
+			//if (ImGui::ShowStyleSelector("Style")) {}
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("View"))
+		{
+			ImGui::Checkbox("Show Demo Window", &showDemoWindow);
+			ImGui::EndMenu();
+		}
+
 
 		//if (showOpenDialog)
 		//{
@@ -140,59 +154,52 @@ void OnGui()
 		//	ImGui::End();
 		//}
 
-		if (ImGui::MenuItem("Quit")) glfwSetWindowShouldClose(window, true);
+		mainMenubarHeight = ImGui::GetWindowHeight();
 		ImGui::EndMainMenuBar();
 	}
 
-	static bool showDemoWindow = true;
-	if (showDemoWindow) ImGui::ShowDemoWindow(&showDemoWindow);
-	
-	{
-		ImGui::Begin("Editor");
+	if(currentSpriteIndex >= 0)
+	{// Editor View (Background)
+		ImGui::SetNextWindowPos(ImVec2(0, mainMenubarHeight));
+		ImGui::SetNextWindowSize(ImVec2((float)windowWidth, windowHeight - mainMenubarHeight));
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+		ImGui::Begin("Editor", nullptr, 
+			ImGuiWindowFlags_NoTitleBar | 
+			//ImGuiWindowFlags_NoBackground | 
+			ImGuiWindowFlags_NoBringToFrontOnFocus |
+			ImGuiWindowFlags_NoMove |
+			ImGuiWindowFlags_NoResize);
 
-		
 
-		//static int currentItem;
-		//const char ** items = new char*[sheet->sprites.size()];
-		//for (size_t i = 0; i < sheet->sprites.size(); i++)
-		//{
-		//	items[i] = "sprite " + (i+1);
-		//}
-
-		//ImGui::ListBox("Sprites", &currentItem, items, sheet, sheet->sprites.size());
-		//if (ImGui::Button("Add")) sheet->sprites.emplace_back();
 		const float maxImageWidth = 500.0f;
-		float imageSize = fmin(ImGui::GetItemRectSize().x, maxImageWidth);
-		imageSize = (ImGui::GetItemRectSize().x / 3) * 2;
+		float imageSize = ImGui::GetContentRegionAvail().y;
 		ImVec2 imageStart = ImGui::GetCursorScreenPos();
 		ImGui::Image((void *)tex, ImVec2(imageSize, imageSize));
-		static float currSpriteRect[4] = {0.25, 0.25, 0.5, 0.5};
-
+		
 		// Settings on the Right or Below
 		{
 			ImGui::SameLine();
 			ImGui::BeginChild("Rect Tools", ImVec2(0, 0), true, ImGuiWindowFlags_AlwaysVerticalScrollbar);
 			ImGui::Text("Sprite Rect: x, y, width, height.");
 			{
-				ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x - 15);
-				ImGui::SliderFloat4("##RectSliders", currSpriteRect, 0.0f, 1.0f);
-				ImGui::InputFloat4("##RectInputs", currSpriteRect, 4);
+				ImGui::PushItemWidth(-1);
+				ImGui::SliderFloat4("##RectSliders", sprites[currentSpriteIndex].rect.data(), 0.0f, 1.0f);
+				ImGui::InputFloat4("##RectInputs", sprites[currentSpriteIndex].rect.data(), 4);
 				ImGui::PopItemWidth();
 			}
 			ImGui::NewLine();
-			static float spriteColor[4] = { 1, 1, 1, 1 };
-			ImGui::ColorEdit4("Color", spriteColor);
+			ImGui::ColorEdit4("Color", sprites[currentSpriteIndex].color.data());
 			const float previewWidth = ImGui::GetItemRectSize().x;
-			ImVec2 size = ImVec2(previewWidth, previewWidth * currSpriteRect[3] / currSpriteRect[2]);
-			ImVec2 uvA = ImVec2(currSpriteRect[0], currSpriteRect[1]);
-			ImVec2 uvB = ImVec2(uvA.x + currSpriteRect[2], uvA.y + currSpriteRect[3]);
-			ImGui::Image((void *)tex, size, uvA, uvB, ImVec4(spriteColor[0], spriteColor[1], spriteColor[2], spriteColor[3]));
+			ImVec2 size = ImVec2(previewWidth, previewWidth * sprites[currentSpriteIndex].rect[3] / sprites[currentSpriteIndex].rect[2]);
+			ImVec2 uvA = ImVec2(sprites[currentSpriteIndex].rect[0], sprites[currentSpriteIndex].rect[1]);
+			ImVec2 uvB = ImVec2(uvA.x + sprites[currentSpriteIndex].rect[2], uvA.y + sprites[currentSpriteIndex].rect[3]);
+			ImGui::Image((void *)tex, size, uvA, uvB, ImVec4(sprites[currentSpriteIndex].color[0], sprites[currentSpriteIndex].color[1], sprites[currentSpriteIndex].color[2], sprites[currentSpriteIndex].color[3]));
 			ImGui::EndChild();
 		}
 
 
-		ImVec2 rectStartPos = ImVec2(imageStart.x + currSpriteRect[0] * imageSize, imageStart.y + currSpriteRect[1] * imageSize);
-		ImVec2 rectEndPos = ImVec2(rectStartPos.x + currSpriteRect[2] * imageSize, rectStartPos.y + currSpriteRect[3] * imageSize);
+		ImVec2 rectStartPos = ImVec2(imageStart.x + sprites[currentSpriteIndex].rect[0] * imageSize, imageStart.y + sprites[currentSpriteIndex].rect[1] * imageSize);
+		ImVec2 rectEndPos = ImVec2(rectStartPos.x + sprites[currentSpriteIndex].rect[2] * imageSize, rectStartPos.y + sprites[currentSpriteIndex].rect[3] * imageSize);
 		auto * drawList = ImGui::GetWindowDrawList();
 		{
 			ImGui::PushClipRect(imageStart, ImVec2(imageStart.x + imageSize, imageStart.y + imageSize), true);
@@ -201,7 +208,32 @@ void OnGui()
 			ImGui::PopClipRect();
 		}
 		ImGui::End();
+		ImGui::PopStyleVar();
 	}
+
+	{
+		ImGui::Begin("Sprite Selector");
+		if (ImGui::Button("Add"))
+		{
+			sprites.emplace_back();
+			currentSpriteIndex = sprites.size() - 1;
+		}
+
+		if (currentSpriteIndex >= 0)
+		{
+			ImGui::SameLine(); 
+			if (ImGui::Button("Remove"))
+			{
+				sprites.erase(sprites.begin() + currentSpriteIndex);
+				currentSpriteIndex--;
+			}
+		}
+		ImGui::ListBox("Sprites", &currentSpriteIndex, &SerializeSprites, sprites.data(), sprites.size(), 16);
+		ImGui::End();
+	}
+
+	if (showDemoWindow) ImGui::ShowDemoWindow(&showDemoWindow);
+
 }
 
 bool OpenTexture(const char * filename)
@@ -214,5 +246,22 @@ bool OpenTexture(const char * filename)
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, x, y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	stbi_image_free(data);
+	return true;
+}
+
+void OpenSprites(const char * filename)
+{
+
+}
+
+void SaveSprites(const char * filename)
+{
+
+}
+
+bool SerializeSprites(void * data, int idx, const char ** outData)
+{
+	std::string *name = new std::string("Sprite " + std::to_string(idx));
+	*outData = name->c_str();
 	return true;
 }
